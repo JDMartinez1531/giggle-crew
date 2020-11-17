1fd0dc4b19be9422d3d0d67180fc56121038022e
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-11-17 13:32:28 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div>
 			<Navigation />
       <Main />
+      <Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./footer.css";
+import Nav from "react-bootstrap/Nav";
+import { FaPhoneAlt, FaFacebook } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+
+const facebookUrl = "https://www.facebook.com";
+const phone = "tel:+5128091305";
+const email = "mailto:regina@gigglecrew.com";
+
+function Footer() {
+	return (
+		<Nav className="footer">
+			<Nav className="footer-icons">
+				<Nav.Item className="footer-rows">
+					<Nav.Link href={facebookUrl}>
+						<FaFacebook />
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item className="footer-rows">
+					<Nav.Link href={phone}>
+						<FaPhoneAlt />
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item className="footer-rows">
+					<Nav.Link href={email}>
+						<AiOutlineMail />
+					</Nav.Link>
+				</Nav.Item>
+			</Nav>
+
+			<Nav className="footer-contact">
+				<p>Phone: 512.809.1305 | Email: Regina@gigglecrew.com</p>
+				<p>Copyright © 2020 Giggle Crew. All rights reserved</p>
+			</Nav>
+		</Nav>
+	);
+}
+
+export default Footer;
